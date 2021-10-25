55e1b88a85e2ed6bff69f614ef3378035cb6ed4e
KeslieBox <kesliebox@gmail.com>
2021-10-24 21:46:24 -0500

    sets up arrows for Carousel

--- upstatement_takehome/src/components/Carousel.js
+++ upstatement_takehome/src/components/Carousel.js
@@ -1,12 +1,28 @@
 import React, { useState } from 'react';
 import { CarouselData } from './CarouselData';
+import {FaArrowAltCircleRight, FaArrowAltCircleLeft} from 'react-icons/fa';
+
 
 export default function Carousel(){
     const [current, setCurrent] = useState(0)
     const length = CarouselData.length
+  
+    const prevSlide = () => {
+        setCurrent(current === 0 ? length - 1 : current - 1)
+    }
+
+    const nextSlide = () => {
+        setCurrent(current === length - 1 ? 0 : current + 1)
+    }
+
+    if(!Array.isArray(CarouselData) || length <= 0) {
+        return null
+    }
 
     return (
         <div className='carousel'>
+            <FaArrowAltCircleLeft id='left-arrow' onClick={prevSlide} size={40}/>
+            <FaArrowAltCircleRight id='right-arrow' onClick={prevSlide} size={40}/>
             {CarouselData.map((slide, index) => {
                 return(
                     <div 
