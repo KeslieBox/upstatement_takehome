6d2456eab02737396a5135880e565dcf250cf8d0
KeslieBox <kesliebox@gmail.com>
2021-11-02 11:09:34 -0500

    refactor/ clean up

--- upstatement_takehome/src/components/Carousel.js
+++ upstatement_takehome/src/components/Carousel.js
@@ -2,15 +2,20 @@ import React, { useState } from 'react';
 import { CarouselData } from './CarouselData';
 import {FaArrowAltCircleRight, FaArrowAltCircleLeft} from 'react-icons/fa';
 
-
 export default function Carousel(){
     const [current, setCurrent] = useState(0)
     const length = CarouselData.length
   
+    // if the prev arrow is clicked & current slide equals 0,
+    // set current slide to the last index of CarouselData 
+    // otherwise, set current slide to the index of the prev slide
     const prevSlide = () => {
         setCurrent(current === 0 ? length - 1 : current - 1)
     }
 
+    // if the next arrow is clicked & current slide is equal to the  
+    // last index of CarouselData, set current slide to 0 
+    // otherwise, set current slide to the prev slide index minus 1
     const nextSlide = () => {
         setCurrent(current === length - 1 ? 0 : current + 1)
     }
@@ -23,15 +28,17 @@ export default function Carousel(){
         <div className='carousel'>
             <FaArrowAltCircleLeft id='left-arrow' onClick={prevSlide} size={40}/>
             <FaArrowAltCircleRight id='right-arrow' onClick={prevSlide} size={40}/>
-            {CarouselData.map((slide, index) => {
+            {CarouselData.map((image, index) => {
                 return(
                     <div 
+                        // if the current index of CarouselData equals the current slide
+                        // set the class name to represent the active slide
                         className={index === current ? 'slide active' : 'slide'} 
                         key={index}
                     >
                         {index === current && (
                             <img 
-                                src={slide.image} 
+                                src={image.url} 
                                 alt='cat' 
                                 className='images' 
                             />
--- upstatement_takehome/src/components/CarouselData.js
+++ upstatement_takehome/src/components/CarouselData.js
@@ -1,17 +1,17 @@
 export const CarouselData = [
     {
-        image: 'https://images.unsplash.com/photo-1589883661923-6476cb0ae9f2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1974&q=80'
+        url: 'https://images.unsplash.com/photo-1589883661923-6476cb0ae9f2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1974&q=80'
     },
     {
-        image: 'https://images.unsplash.com/photo-1541781774459-bb2af2f05b55?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1160&q=80'
+        url: 'https://images.unsplash.com/photo-1541781774459-bb2af2f05b55?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1160&q=80'
     },
     {
-        image: 'https://images.pexels.com/photos/416160/pexels-photo-416160.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+        url: 'https://images.pexels.com/photos/416160/pexels-photo-416160.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
     },
     {
-        image: 'https://images.pexels.com/photos/730537/pexels-photo-730537.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+        url: 'https://images.pexels.com/photos/730537/pexels-photo-730537.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
     },
     {
-        image: 'https://images.pexels.com/photos/4602/jumping-cute-playing-animals.jpg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+        url: 'https://images.pexels.com/photos/4602/jumping-cute-playing-animals.jpg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
     }
 ]
